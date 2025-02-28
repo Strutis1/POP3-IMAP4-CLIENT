36738572654813430edd3f8e567dd036e5bd6104
Bielskis Vėjas <vejas.bielskis@mif.stud.vu.lt>
2025-02-28 20:54:46 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.client.pop3imap4client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.client.pop3imap4client to javafx.fxml;
+    exports com.client.pop3imap4client;
+}
